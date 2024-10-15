e605771c999b239facaf51388abc8bb552864254
Elanora Manson <git@elanora.lol>
2024-10-14 21:15:11 -0700

    Flake cross platform-ish and a bit nicer, README

---
+++ README.md
@@ -0,0 +1,11 @@
+# [elanora.lol](https://elanora.lol)
+
+## My personal site
+
+This site is something between a fun project and a professional necessity
+
+I plan on using it to push forward my hobbies and to advertise my developer expertise or something
+
+Currently (2024-10-14)\* hosted on GH pages, this site is an SSR ready Vite powered Typescripted React Router routed MarkdownX'd smorgasbord of front end experience. I'm also rather proud of all the eslint, prettier, stylelint rules that are setup and checked at commit time. For once my personal site looks kind of like I know what I'm doing??
+
+If you want to see what it's like to dev this page, I've provided a nix flake with a dev shell and all the dependencies needed
--- flake.nix
+++ flake.nix
@@ -1,23 +1,52 @@
 {
-  description = "elanora.lol devshell";
+  description = "elanora.lol devShell";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
-    { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in
     {
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = [
-          pkgs.nodejs_22
-          pkgs.nodePackages.typescript
-          pkgs.nodePackages.typescript-language-server
-        ];
-      };
-    };
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nodejs_22
+            pkgs.nodePackages.typescript
+            pkgs.nodePackages.typescript-language-server
+          ];
+
+          shellHook = ''
+            confirm() {
+              while true; do
+                read -rp "[y/n]: " yn
+                case $yn in
+                  [Yy]*) return 0;;
+                  [Nn]*) return 1;;
+                  *) echo "Please answer yes or no." ;;
+                esac
+              done
+            }
+            cat ./banner.txt
+
+            if [ ! -d "./node_modules" ]; then
+              echo "$(pwd)/node_modules does not seem to exist"
+              echo -n "Run npm install? "
+              if confirm; then
+                npm install
+              fi
+            fi
+          '';
+        };
+      }
+    );
 }
