43073707bfc19699048dea26a13f8eeabb57e4d8
Elanora Manson <git@elanora.lol>
2026-07-13 09:32:36 -0700

    Flake revision, add git-hooks-nix, add treefmt-nix, lock update

--- flake.nix
+++ flake.nix
@@ -3,30 +3,53 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+
+    # keep-sorted start block=yes
+
+    # Simplify Nix Flakes with the module system
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
-    systems.url = "github:nix-systems/default";
+    # Integration of pre-commit git hooks with Nix
+    git-hooks-nix = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    # treefmt for nix
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # keep-sorted end
   };
 
   outputs =
     inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = import inputs.systems;
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+      imports = [
+        inputs.git-hooks-nix.flakeModule
+        inputs.treefmt-nix.flakeModule
+      ];
       perSystem =
         {
           self',
           pkgs,
           lib,
+          config,
           ...
         }:
         let
           name = "elanora.lol";
           pname = name;
           src = ./.;
-          inherit (pkgs) buildNpmPackage;
-          inherit (pkgs) importNpmLock;
+          inherit (pkgs) buildNpmPackage importNpmLock;
           nodejs = pkgs.nodejs_latest;
 
           meta = {
@@ -62,9 +85,33 @@
             default = self'.packages.frontend;
           };
 
+          pre-commit = {
+            check.enable = true;
+            settings.hooks = {
+              treefmt.enable = true;
+            };
+          };
+
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs = {
+              # keep-sorted start block=yes
+              # biome.enable = true;
+              deadnix.enable = true;
+              keep-sorted.enable = true;
+              mdformat.enable = true;
+              nixfmt.enable = true;
+              statix.enable = true;
+              typos.enable = true;
+              # keep-sorted end
+            };
+          };
+
           devShells.default = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              nixfmt-rfc-style
+            name = "${name}-shell";
+            inputsFrom = [
+              config.treefmt.build.devShell
+              config.pre-commit.devShell
             ];
             packages = [
               importNpmLock.hooks.linkNodeModulesHook
